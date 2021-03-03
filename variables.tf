a3202abb0afae9115aaf306ae5ce23d80cd38635
ASC IT Solutions <andrew@asc-solutions.co.uk>
2021-03-03 13:14:16 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,15 @@
-variable "vnet_resource_group_name" {
-  description = "The name of the Resource group where the vNET for the Private Endpoint exists."
-  type        = string
+variable "pe_network" {
+  description = "The required Azure networking details for the new Private Endpoint NIC."
+  type = object({
+    resource_group_name = string
+    vnet_name           = string
+    subnet_name         = string
+    )}
 }
 
 variable "pe_resource_group_name" {
-  description = "The name of the Resource group where the the Private Endpoint will be created. Typically the same Rg as the resource needing the endpoint."
+  description = "The name of the Resource group where the the Private Endpoint resource will be created."
   type        = string
-}
-
-variable "vnet_name" {
-  description = "The name of the vNet where the Private Endpoint subnet is located."
-  type        = string
-}
-
-variable "subnet_name" {
-  description = "The name of the subnet where the Private Endpoint will be created."
-  type        = string
-}
-
-variable "subresource_names" {
-  description = "A list of subresources that the private Endpoint is able to connect to. Refer to Azure documentation for full list."
-  type        = list(string)
 }
 
 variable "dns" {
@@ -30,9 +19,7 @@ variable "dns" {
     )}
 }
 
-variable "endpoint_resource" {
-  type = object({
-    id   = string
-    name = string
-    )}
+variable "endpoint_resource_id" {
+  description = "The ID of the resource that the new Private Endpoint will be assigned to."
+  type        = string
 }
